36367396dd6508e22937bae21a8a7a097c32eed4
ClickSweden <clicksweden@hotmail.com>
2024-03-16 04:57:59 +0100

    Create 02_dataTypes.js

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";   // Treat all JS code as newer version
+
+// alert(3+3);  // We are using nodeJS, not browser
+
+console.log(3 
+    + 
+     3)  // Code readability should be high
+
+console.log("Shezi")
+
+let name = "Shezi"
+let age = 20
+let isLoggedIn = true
+let state
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>  
+// symbol => unique 
+
+// OBJECT
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null); // object
