09f5a78d5079d0539b45e43ddb48e544cc1eed4e
ClickSweden <clicksweden@hotmail.com>
2024-03-19 04:52:12 +0100

    Create 04_comparision.js

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,23 @@
+
+// Comparisons
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0);  // => false
+// console.log(null == 0); // => false
+// console.log(null >= 0); // => true
+
+// console.log(undefined > 0);  // => false 
+
+/*  STRICT CHECK  */
+// ===
+
+console.log("2" == 2);  // => true
+console.log("2" === 2); // => false
